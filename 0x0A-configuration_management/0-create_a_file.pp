0df5f164c5ebb8d78fe24a28cc7c6c16bdda9d62
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-09-16 13:01:52 +0100

    Create 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# Creates a file
-file {'/tmp/school':
-    mode    => '0744'
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet'
+# A file is created using puppet in /tmp
+file { '/tmp/school':
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
 }
